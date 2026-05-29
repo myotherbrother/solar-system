81e1197c110163bdb570b6f234fe765495b42b3f
myotherbrother <darrel.gill@outlook.com>
2026-05-29 13:10:32 -0600

    update app and package.json to support  mongoose 18.0.0

--- app.js
+++ app.js
@@ -13,16 +13,10 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
-    }
-})
+    pass: process.env.MONGO_PASSWORD
+}).catch((err) =>{
+        console.log("error!! " + err);
+});
 
 var Schema = mongoose.Schema;
 
@@ -38,19 +32,19 @@ var planetModel = mongoose.model('planets', dataSchema);
 
 
 
-app.post('/planet',   function(req, res) {
+app.post('/planet',  async function(req, res) {
    // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
-        } else {
-            res.send(planetData);
-        }
-    })
-})
+    try {
+        const planetData = await planetModel.findOne({
+            id: req.body.id
+        });
+
+        res.send(planetData);
+    } catch (err) {
+        console.log("Error in Planet Data", err);
+        res.status(500).send("Error in Planet Data");
+    }
+});
 
 app.get('/',   async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
